f6d8129905dd364a3647abecbcbd350bc94a3ab6
hooN <johnhpark11@gmail.com>
2020-04-26 01:41:50 +0900

    binary/nominal/ordinal logistic regression

--- (4)logistic_regression.R
+++ (4)logistic_regression.R
@@ -1,10 +1,11 @@
 
 #***Binary Logistic Regression
 
-
 d.admit <- read.csv("(4)logistic_regression_admission.csv")
 logr <- glm(admit ~ gre+gpa+rank, data=d.admit, family=binomial("logit"))
 summary(logr)
+
+
 
 #***Nominal Logistic Regression
 #ln(P_j/P_J) = exp(alpha_j + beta_j * X)
@@ -12,11 +13,18 @@ summary(logr)
 d.brand <- read.csv("(4)logistic_regression_brandchoice.csv", header=TRUE, sep=',')
 head(d.brand)
 library(mlogit) #need installation >install.packages(mlogit)
-#d.brand$brand <- as.factor(d.brand$brand)
-#d.brand$brand
 
 d.brand$brand <- factor(d.brand$brand)
 td.brand <- mlogit.data(d.brand, varying=NULL, choice="brand", shape="wide")
 head(td.brand)
 nlogr <- mlogit(brand~1|female+age, data=td.brand, reflevel="1")
 summary(nlogr)
+
+
+#***Ordinal Logistic Regression
+#logit(P(Y<=j)) = alpha_j + beta*X, j=1,2,...J-1, beta is a constant.
+d.gograd <- read.csv("(4)logistic_regression_gograd.csv")
+head(d.gograd)
+library(VGAM)
+ologr<-vglm(apply~pared+public+gpa, cumulative(parallel=TRUE, reverse=FALSE), d.gograd)
+summary(ologr)
